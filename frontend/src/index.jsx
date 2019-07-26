aca01ff854be839bf9e3d08c14fa8ae78ee8a375
elton-develcode <elton.develcode@develcode.com.br>
2019-07-26 16:40:34 -0300

    Acabando o dia

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import ReacDOM from 'react-dom'
-import App from './main/app'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-ReacDOM.render(<App />, document.getElementById('app'))
+import App from './main/app'
+import reducers from './main/reducers'
+
+const store = createStore(reducers)
+
+ReacDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>  
+  , document.getElementById('app'))
