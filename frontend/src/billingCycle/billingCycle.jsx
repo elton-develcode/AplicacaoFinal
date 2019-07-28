30a9e6fda2d8a2273ada34a5daf2435c506430e5
elton-develcode <elton.veiga@develcode.com.br>
2019-07-28 20:12:09 -0300

    Avancado nas funcionalidades

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -9,12 +9,16 @@ import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent' 
-import { selectTab } from '../common/tab/tabAction'
+import { selectTab, showTabs } from '../common/tab/tabAction'
+import List from '../billingCycle/billingCycleList'
+import Form from './billingCycleForm'
+import { create } from './billingCycleActions'
 
 class BillingCycle extends Component {
 
 componentWillMount(){
   this.props.selectTab('tabList')
+  this.props.showTabs('tabList', 'tabCreate')
 }
 
   render(){
@@ -30,8 +34,12 @@ componentWillMount(){
                 <TabHeader label='Incluir' icon='trash-o' target='tabDelete' />
               </TabsHeader>
               <TabsContent>
-                <TabContent id='tabList'><h1>Lista</h1></TabContent>
-                <TabContent id='tabCreate'><h1>Criar</h1></TabContent>
+                <TabContent id='tabList'>
+                  <List />
+                </TabContent>
+                <TabContent id='tabCreate'>
+                  <Form onSubmit={this.props.create}/>
+                </TabContent>
                 <TabContent id='tabUpdate'><h1>Editar</h1></TabContent>
                 <TabContent id='tabDelete'><h1>Deletar</h1></TabContent>
               </TabsContent>
@@ -42,5 +50,7 @@ componentWillMount(){
   }
 }
 
-const mapDispatchToProps = dispacth => bindActionCreators({selectTab}, dispacth)
+const mapDispatchToProps = dispacth => bindActionCreators({
+  selectTab, showTabs, create
+}, dispacth)
 export default connect(null,mapDispatchToProps)(BillingCycle)
--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,11 +1,17 @@
 import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
+import { reducer as toastrReducer } from 'react-redux-toastr'
 
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
+import BillingCycleReducer from '../billingCycle/billingCycleReducer'
 
 const rootReducers = combineReducers({
     dashboard: DashboardReducer,
-    tab: TabReducer
+    tab: TabReducer,
+    BillingCycle: BillingCycleReducer,
+    form: formReducer,
+    toastr: toastrReducer
 })
 
 export default rootReducers
